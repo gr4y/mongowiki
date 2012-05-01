4482b671c600e6ef64a9144c0fa74cce27a059ec
Sascha Wessel <swessel@gr4yweb.de>
2012-05-01 17:33:50 +0200

    added validations to the model. I still have to render the validation errors, later.

--- lib/mongowiki/models/article.rb
+++ lib/mongowiki/models/article.rb
@@ -1,11 +1,15 @@
 class Article
   
   include Mongoid::Document
-  include Mongoid::Search
+  include Mongoid::Search  
   
   field :title, :type => String
   field :text
 
   search_in :title, :text
+
+  validates_presence_of :title
+  validates_uniqueness_of :title
+  validates_presence_of :text
 
 end
