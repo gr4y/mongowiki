0705ca75acf4a0fd3e2d65ef069387a5fe092fbd
Sascha Wessel <swessel@gr4yweb.de>
2012-05-05 16:58:30 +0200

    sprockets logging only when logging is true

--- lib/mongowiki/app.rb
+++ lib/mongowiki/app.rb
@@ -8,7 +8,7 @@ module MongoWiki
     
     # sinatra
     set :show_exceptions, true
-    set :logging, true
+    set :logging, false
   
     # sprockets    
     set :sprockets, Sprockets::Environment.new(root)
@@ -22,14 +22,16 @@ module MongoWiki
       sprockets.append_path File.join(assets_path, "stylesheets")
       sprockets.append_path File.join(assets_path, "javascripts")
       sprockets.append_path File.join(assets_path, "images")
-      sprockets.logger = Logger.new(STDOUT)
+      if logging
+        sprockets.logger = Logger.new($stdout)
+        Mongoid.logger = Logger.new($stdout)
+      end
           
       # Configure Sprockets::Helpers (if necessary)
       Sprockets::Helpers.configure do |config|
         config.environment = sprockets
         config.prefix      = assets_prefix
         config.digest      = digest_assets
-        config.public_path = public_folder
       end
       
       Rabl.register!
