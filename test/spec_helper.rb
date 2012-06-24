639daedb9e4eac924e9b8e35b56ec8149dd03279
Sascha Wessel <swessel@gr4yweb.de>
2012-06-24 18:30:23 +0200

    removed test_helper and unit tests. we now use minitest spec

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -1,6 +1,31 @@
-require 'test_helper'
+require 'minitest/autorun'
+require 'rack/test'
+require 'database_cleaner'
 require 'mongoid-minitest'
+require 'mongowiki'
+require 'yaml'
+
+# if the suite is not running on travis, 
+# read the url to mongodb from test/mongodb.yml 
+if !ENV['TRAVIS']
+  yaml = YAML::load_file('test/mongodb.yml')
+  url=yaml['url']
+  Mongoid::Config.from_hash("uri" => url)
+end
+
+# setup DatabaseCleaner
+DatabaseCleaner.strategy = :truncation
 
 class MiniTest::Spec
   include Mongoid::Matchers
+  include Rack::Test::Methods
+  
+  before :each do
+    DatabaseCleaner.start
+  end
+
+  after :each do
+    DatabaseCleaner.clean
+  end
+  
 end
--- test/test_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'minitest/autorun'
-require 'rack/test'
-require 'mongowiki'
-require 'yaml'
-
-if !ENV['TRAVIS']
-  yaml = YAML::load_file('test/mongodb.yml')
-  ENV['MONGO_URL']=yaml['url']
-end
