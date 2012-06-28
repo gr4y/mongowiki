865722dc1165d5d6f5cfb3838120004447e9dee3
Sascha Wessel <swessel@gr4yweb.de>
2012-06-28 18:54:25 +0200

    added Mongoid::Versioning to article

--- lib/mongowiki/models/article.rb
+++ lib/mongowiki/models/article.rb
@@ -3,13 +3,17 @@ class Article
   
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Mongoid::Versioning
   include Mongoid::Search
   
+  # document
   field :title, :type => String
   field :text
 
+  # search
   search_in :title, :text
 
+  # validations
   validates_presence_of :title
   validates_uniqueness_of :title
   validates_presence_of :text
