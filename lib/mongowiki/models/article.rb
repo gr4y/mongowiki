7659964987731dfc21a533e7a5bb2f1495c67941
Sascha Wessel <swessel@gr4yweb.de>
2012-06-30 12:00:16 +0200

    reformatted code.

--- lib/mongowiki/models/article.rb
+++ lib/mongowiki/models/article.rb
@@ -9,13 +9,13 @@ class Article
   # document
   field :title, :type => String
   field :text
-
+  
   # search
   search_in :title, :text
-
+  
   # validations
   validates_presence_of :title
   validates_uniqueness_of :title
   validates_presence_of :text
-
+  
 end
