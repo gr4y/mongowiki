9aa9d46eea837d0df009b8e54c130cee77c2efb0
Sascha Wessel <swessel@gr4yweb.de>
2012-07-29 13:06:31 +0200

    added initializers. same hideous code like the controllers requiring.

---
+++ lib/mongowiki/initializers/mongoid.rb
@@ -0,0 +1,9 @@
+# set mongoid logger in production
+Mongoid.logger.level = 3 if Happy.env.production?
+
+# connect mongoid to the mongodb instance
+if mongo_uri = ENV['MONGO_URL']
+  Mongoid::Config.from_hash("uri" => mongo_uri)
+else
+  raise "MONGO_URL environment variable not set"
+end
