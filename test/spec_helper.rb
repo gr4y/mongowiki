9d6810bd387b66f9fcbfcbf1771005b0a45de0dd
Sascha Wessel <swessel@gr4yweb.de>
2012-07-29 13:17:52 +0200

    fixed spec_helper

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -1,26 +1,8 @@
 require 'minitest/autorun'
-require 'rack/test'
-require 'database_cleaner'
 require 'mongoid-minitest'
-require 'mongowiki'
+require 'database_cleaner'
+require 'rack/test'
 require 'yaml'
-
-ENV['MONGO_URL']='mongodb://localhost/mongowiki_test'
-
-def mongo_url 
-  url = ENV['MONGO_URL']
-  if !url
-    if !::File.exists?('test/mongodb.yml')
-      raise 'MONGO_URL environment variable not set and test/mongodb.yml not found'
-    end
-    yaml = YAML::load_file('test/mongodb.yml')
-    url = yaml['url']
-  end  
-  url
-end
-
-# set mongo_url 
-Mongoid::Config.from_hash("uri" => mongo_url)
 
 # setup DatabaseCleaner
 DatabaseCleaner.strategy = :truncation
@@ -38,3 +20,18 @@ class MiniTest::Spec
   end
   
 end
+
+def mongo_url 
+  url = ENV['MONGO_URL']
+  if !url
+    if !::File.exists?('test/mongodb.yml')
+      raise 'MONGO_URL environment variable not set and test/mongodb.yml not found'
+    end
+    yaml = YAML::load_file('test/mongodb.yml')
+    url = yaml['url']
+  end  
+  url
+end
+ENV['MONGO_URL'] = mongo_url
+
+require 'mongowiki'
